b2ace377e450daaefb17c04bb0b25efebbb25961
Nhoya <nhoyadx@gmail.com>
2017-08-16 15:01:32 +0200

    added direct call to haveibeenpwnd and pgp search module

--- gOSINT.go
+++ gOSINT.go
@@ -8,9 +8,18 @@ import (
 )
 
 var opts struct {
-	Module     string `short:"m" long:"module" description:"Specify module" required:"true" choice:"pgp" choice:"pwn" choice:"whois" choice:"gitAPI" choice:"git"`
+	Module     string `short:"m" long:"module" description:"Specify module" required:"true" choice:"pgp" choice:"pwnd"  choice:"git"`
 	Url        string `long:"url" default:"" description:"Specify target URL"`
-	gitAPIType string `long:"gitAPI" default "" description:"Specify git website API to use" choice:"github" choice"bitbucket"`
+	gitAPIType string `long:"gitAPI" default:"" description:"Specify git website API to use" choice:"github" choice"bitbucket"`
+	Mail       string `long:"mail" default:"" description:"Specify mail target"`
+}
+
+func mailCheck(mailSet mapset.Set) {
+	if opts.Mail == "" {
+		fmt.Println("You must specify target mail")
+		os.Exit(1)
+	}
+	mailSet.Add(opts.Mail)
 }
 
 func main() {
@@ -19,15 +28,21 @@ func main() {
 	_, err := flags.Parse(&opts)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	switch mod := opts.Module; mod {
+	case "pwnd":
+		mailCheck(mailSet)
+		pwnd(mailSet)
+	case "pgp":
+		mailCheck(mailSet)
+		pgpSearch(mailSet)
 	case "git":
 		if opts.Url == "" {
 			fmt.Println("You must specify target URL")
 			os.Exit(1)
 		}
-
 		mailSet = gitSearch(opts.Url, opts.gitAPIType, mailSet)
 		mailSet = pgpSearch(mailSet)
 		pwnd(mailSet)
