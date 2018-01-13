afdf533b0300b805a4501b465050856dda51dafe
Nhoya <nhoyadx@gmail.com>
2018-01-14 00:57:57 +0100

    Fixed bad handling of quote, added sleep inside the telegram parsing function to prevent blocking

--- telegram.go
+++ telegram.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"time"
 )
 
 func getTelegramGroupHistory(group string, grace int) {
@@ -30,17 +31,26 @@ func getTelegramGroupHistory(group string, grace int) {
 			}
 		}
 		i++
+		time.Sleep(time.Millisecond * 500)
 	}
 }
 
 func getTelegramMessage(body string) string {
 	re := regexp.MustCompile(`class=\"tgme_widget_message_text\" dir=\"auto\">(.*)<\/div>\n`)
-	match := re.FindStringSubmatch(body)
-	if len(match) == 2 {
-		return match[1]
-	} else {
-		return ""
+	//match := re.FindStringSubmatch(body)
+	//if len(match) == 2 {
+	//	return match[1]
+	//} else {
+	//	return ""
+	//}
+	match := re.FindAllStringSubmatch(body, -1)
+	if len(match) == 1 {
+		return match[0][1]
+	} else if len(match) == 2 {
+		return ">" + match[0][1] + "\n" + match[1][1]
 	}
+	return ""
+
 }
 
 func getTelegramUsername(body string) (string, string) {
