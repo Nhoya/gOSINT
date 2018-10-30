1861fbcd6465d1985030a55e19cb6abb8866280e
Giovanni Spera <giovanni.spera2011@libero.it>
2018-10-30 13:29:54 +0100

    Changed darwin config path to a more idiomatic one Config path moved to: /Library/Application Support/gOSINT Renamed internal/utils/vars_macos.go to internal/utils/vars_darwin.go

--- internal/telegram/vars_darwin.go
+++ internal/telegram/vars_darwin.go
@@ -4,5 +4,5 @@ import "os"
 
 var (
 	// TelegramDumpPath defines the path for delegram dumps
-	TelegramDumpPath = os.Getenv("HOME") + "/.local/share/gOSINT/tgdumps/"
+	TelegramDumpPath = os.Getenv("HOME") + "/Library/Application Support/gOSINT/tgdumps/"
 )
--- internal/utils/vars_darwin.go
+++ internal/utils/vars_darwin.go
@@ -4,5 +4,5 @@ import "os"
 
 var (
 	// ConfigFilePath defines the path where the standard configuration file will be written
-	ConfigFilePath = os.Getenv("HOME") + "/.config/"
+	ConfigFilePath = os.Getenv("HOME") + "/Library/Application Support/gOSINT/config/"
 )
