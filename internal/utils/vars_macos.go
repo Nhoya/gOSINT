4d81c06e6206b090af525814c526eeb4dd3a041e
Giovanni Spera <giovanni.spera2011@libero.it>
2018-10-30 12:47:16 +0100

    Added vars_darwin.go to utils telegram vars_darwin.go are just copy of vars_linux.go because $HOME is present also in macOS.

---
+++ internal/utils/vars_macos.go
@@ -0,0 +1,8 @@
+package utils
+
+import "os"
+
+var (
+	// ConfigFilePath defines the path where the standard configuration file will be written
+	ConfigFilePath = os.Getenv("HOME") + "/.config/"
+)
